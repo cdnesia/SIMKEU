e9a06d368571cf26c6b406979f8a91cafea1aef2
mjumain93 <mjumain93@gmail.com>
2026-02-27 16:40:07 +0700

    perbaikan sync pmb

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ Route::get('/test', function () {
 });
 
 Route::post('/cek-tagihan', [TagihanController::class, 'cekTagihanFromSimawa'])->middleware('verifyHmac');
+Route::post('/cek-tagihan-pmb', [TagihanController::class, 'cekPembayaranPMB'])->middleware('verifyHmac');
 Route::post('/generate-tagihan', [TagihanController::class, 'generateTagihanFromSimawa'])->middleware('verifyHmac');
 Route::post('/riwayat-pembayaran', [TagihanController::class, 'riwayatPembayaran'])->middleware('verifyHmac');
 Route::post('/cek-kontrak-matakuliah', [TagihanController::class, 'cekKontrakMk'])->middleware('verifyHmac');
