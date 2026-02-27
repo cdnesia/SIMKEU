6b3d4bdd656660d311352f2ab0988cdac32979c0
mjumain93 <mjumain93@gmail.com>
2026-02-27 09:07:21 +0700

    update dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,6 @@ class HomeController extends Controller
 {
     public function index(CronSyncService $cronSyncService)
     {
-        dd($cronSyncService->SyncPembayaranNew());
         return view('home');
     }
 }
